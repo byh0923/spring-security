d106e40426ef2c402cdb58690e376c77b8d54f7f
YoungHyun Bae <byh2949@gmail.com>
2022-04-21 23:34:01 +0900

    spring security 구현 코드, 테스트 코드 작성

---
+++ src/main/java/com/example/springsecurity/account/AccountService.java
@@ -0,0 +1,42 @@
+package com.example.springsecurity.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+    /**
+     * dao가 와도 괜찮다.
+     * */
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if(account == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        // account를 userdetails 타입으로 바꿔야함
+        // 스프링 시큐리티에서 User를 제공해줌
+        return User.builder()
+                .username(account.getUsername())
+                .password(account.getPassword())
+                .roles(account.getRole())
+                .build();
+    }
+
+    public Account createNew(Account account) {
+        account.encodePassword(passwordEncoder);
+        return this.accountRepository.save(account);
+    }
+}
---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.springsecurity.config;
+
+import com.example.springsecurity.account.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+    }
+}
